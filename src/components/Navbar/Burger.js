0615b54df74fa29f5cd19474031092c1086980be
amin <aminqanbarii1992@gmail.com>
2022-04-17 08:31:21 -0700

    create burger component

---
+++ src/components/Navbar/Burger.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+
+//styled-components
+import styled from "styled-components";
+import NavbarSmall from './NavbarSmall';
+
+const Div = styled.div`
+  display: none;
+  width: 2rem;
+  height: 1.8rem;
+  position: absolute;
+  top: 20px;
+  right: 47px;
+  z-index: 200;
+  cursor: pointer;
+  
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+  }
+  @media (max-width: 380px) {
+    width: 28px;
+    height: 28px;
+    right: 38px;
+   }
+  div {
+    width: 1.9rem;
+    height: 0.23rem;
+    background-color: ${({ open }) => (open ? "#f1f1f1" : "#fff")};
+    border-radius: 10px;
+    transform-origin: 1.8px;
+    transition: all 0.2s linear;
+    :nth-child(1) {
+      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+    }
+    :nth-child(2) {
+      transform: ${({ open }) => (open ? "translateX(100%)" : "translateX(0)")};
+      opacity: ${({ open }) => (open ? 0 : 1)};
+    }
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+    }
+    @media (max-width: 380px) {
+      width: 1.5rem;
+      height: 0.2rem;
+    transform-origin: 0px;
+
+  
+
+     }
+  }
+`;
+
+const Burger = () => {
+    const [open, setOpen] = useState(false);
+  const openHandler = () => {
+    setOpen(!open);
+  };
+
+  const closeHandler = () => {
+    setOpen(false)
+  }
+  return (
+    <div>
+        <Div open={open} onClick={openHandler}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Div>
+      <NavbarSmall open={open} close={closeHandler}/>
+    </div>
+  )
+}
+
+export default Burger
