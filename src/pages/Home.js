b1338b66a93555c906332f8773db71cadefac955
amin <aminqanbarii1992@gmail.com>
2022-04-19 06:24:08 -0700

    add JumpToUp component in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Announcement from '../components/Announcement'
 import Categories from '../components/Categories'
+import JumpToUp from '../components/JumpToUp'
 import Newsletter from '../components/Newsletter'
 import PopularProducts from '../components/PopularProducts'
 import Slider from '../components/Slider'
@@ -13,6 +14,7 @@ const Home = () => {
         <Categories/>
         <PopularProducts/>
         <Newsletter/>
+        <JumpToUp/>
     </div>
   )
 }
