dba215b3b285fb00d707dc8d07c4743dba01d55b
amin <aminqanbarii1992@gmail.com>
2022-04-19 05:05:18 -0700

    edit navbar small styles and add <hr/>

--- src/components/Navbar/NavbarSmall.js
+++ src/components/Navbar/NavbarSmall.js
@@ -10,7 +10,6 @@ import { Link } from "react-router-dom";
 const Ul = styled.ul`
   direction: rtl;
   width: 40%;
-  height: 100%;
   display: none;
   flex-direction: column;
   align-items: center;
@@ -18,18 +17,26 @@ const Ul = styled.ul`
   top: -20px;
   right: 0;
   bottom: 0;
-  /* left: 0; */
-  z-index: 110;
+  /* z-index: 110; */
   padding-top: 50px;
-  background-color: #fff;
-  /* opacity: 0.9; */
+  background-color: #ffffff;
   transition: all 0.8s linear;
+  margin-top: 20px;
+
+  hr{
+    margin: 0px;
+    flex-shrink: 0;
+    border-width: 0px 0px thin;
+    border-style: solid;
+    border-color: rgba(0, 0, 0, 0.12);
+    z-index: 20000;
+    width: 100%;
+  }
 
   @media (max-width: 768px) {
     display: ${({ open }) => (open ? "flex" : "none")};
   }
   li {
-right: 0;
     transition: background-color 0.1s linear;
     padding: 10px 0;
     color: gray;
@@ -37,7 +44,7 @@ right: 0;
     justify-content: center;
     align-items: center;
     cursor: pointer;
-    width: 100%;
+    width:100%;
     a {
       cursor: pointer;
       font-size: 14px;
@@ -55,8 +62,7 @@ right: 0;
       transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
     }
     &:hover {
-      background-color: #ccc;
-      color: black;
+      background-color: #F5F5F5;
     }
     svg {
       padding-left: 10px;
@@ -81,6 +87,7 @@ const NavbarSmall = ({ open, close }) => {
         <Link to="/login"><AssignmentInd/>ثبت نام</Link>
 
         </li>
+      <hr />
       </Ul>
     </div>
   );
