67ca3dce5b695e7afbc7a4007ae00bc6a692012a
amin <aminqanbarii1992@gmail.com>
2022-04-20 02:23:37 -0700

    responsive this component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,14 +7,14 @@ import { Add, Remove, Delete } from "@material-ui/icons";
 //responsive
 import { mobile } from "../responsive";
 
-//helper 
+//helper
 import { quantityCount } from "../helper/function";
 
 //useSelector and useDispatch
 import { useDispatch, useSelector } from "react-redux";
 
 //cart action
-import { removeItem , increase , decrease  } from "../Redux/Cart/cartAction";
+import { removeItem, increase, decrease } from "../Redux/Cart/cartAction";
 
 const Info = styled.div`
   flex: 3;
@@ -30,19 +30,39 @@ const Product = styled.div`
   border-radius: 3px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 4px 12px -2px,
     rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
-  ${mobile({ flexDirection: "column" })}
+
+  @media (max-width: 576px) {
+    flex-direction: column;
+  }
 `;
 
 const ProductDetail = styled.div`
   flex: 2;
   display: flex;
   justify-content: space-between;
+  @media (max-width: 380px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 const Image = styled.img`
   width: 200px;
   height: 200px;
   margin: 10px;
+  @media (max-width: 768px) {
+    width: 175px;
+    height: 175px;
+  }
+  @media (max-width: 576px) {
+    width: 175px;
+    height: 175px;
+  }
+  @media (max-width: 380px) {
+    width: 200px;
+    height: 200px;
+  }
 `;
 
 const Details = styled.div`
@@ -51,9 +71,22 @@ const Details = styled.div`
   flex-direction: column;
   direction: rtl;
   justify-content: space-around;
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
 `;
 
-const ProductName = styled.span``;
+const ProductName = styled.span`
+  padding-bottom: 10px;
+  @media (max-width: 576px) {
+    display: flex;
+    flex-direction: column;
+  }
+  @media (max-width: 380px) {
+    display: flex;
+    flex-direction: row;
+  }
+`;
 
 const ProductSize = styled.span``;
 
@@ -72,6 +105,8 @@ const ProductAmountContainer = styled.div`
 `;
 
 const AddContainer = styled.div`
+  display: flex;
+  align-items: center;
   color: green;
   cursor: pointer;
   transition: all 0.1s ease;
@@ -82,6 +117,8 @@ const AddContainer = styled.div`
 `;
 
 const RemoveContainer = styled.div`
+  display: flex;
+  align-items: center;
   color: red;
   cursor: pointer;
   transition: all 0.1s ease;
@@ -91,6 +128,8 @@ const RemoveContainer = styled.div`
   }
 `;
 const TrashContainer = styled.div`
+  display: flex;
+  align-items: center;
   cursor: pointer;
   transition: all 0.1s ease;
   border-radius: 100%;
@@ -101,11 +140,14 @@ const TrashContainer = styled.div`
 
 const ProductAmount = styled.div`
   font-size: 20px;
-  margin: 5px 10px;
+  margin: 5px 8px;
   background-color: #fff;
   border: 1px solid lightgray;
   border-radius: 4px;
   padding: 0px 8px;
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
   ${mobile({ margin: "5px 15px" })}
 `;
 
@@ -113,6 +155,13 @@ const ProductPrice = styled.div`
   font-size: 20px;
   font-weight: 200;
   direction: rtl;
+  @media (max-width: 768px) {
+    font-size: 17px;
+  }
+  @media (max-width: 576px) {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
 
   ${mobile({ marginBottom: "20px" })}
 `;
@@ -120,10 +169,8 @@ const ProductPrice = styled.div`
 const Cart = ({ data }) => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
-  const { img, price, title , id } = data;
+  const { img, price, title, id } = data;
 
-
-  
   return (
     <Info>
       <Product>
@@ -135,28 +182,22 @@ const Cart = ({ data }) => {
             </ProductName>
 
             <ProductSize>
-              <b>Size:</b> 37.5
+              <b>سایز :</b> 37.5
             </ProductSize>
           </Details>
         </ProductDetail>
         <PriceDetail>
           <ProductAmountContainer>
-            <AddContainer
-              onClick={() => dispatch(increase(data))}
-            >
+            <AddContainer onClick={() => dispatch(increase(data))}>
               <Add />
             </AddContainer>
-            <ProductAmount>{quantityCount(state , id)}</ProductAmount>
-            {quantityCount(state , id) > 1 ? (
-              <RemoveContainer
-                onClick={() => dispatch(decrease(data))}
-              >
+            <ProductAmount>{quantityCount(state, id)}</ProductAmount>
+            {quantityCount(state, id) > 1 ? (
+              <RemoveContainer onClick={() => dispatch(decrease(data))}>
                 <Remove />
               </RemoveContainer>
             ) : (
-              <TrashContainer
-                onClick={() => dispatch(removeItem(data))}
-              >
+              <TrashContainer onClick={() => dispatch(removeItem(data))}>
                 <Delete />
               </TrashContainer>
             )}
